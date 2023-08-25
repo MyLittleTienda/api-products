23ddbff337c8ceedb587d781dc098f9959d5bcdd
RustyCrazyPunky <crazypunky@protonmail.com>
2023-08-24 22:58:39 -0300

    feat: Se agregan servicios para agregar y eliminar precios, categorias e imagenes de los productos.

--- src/main/java/com/mlt/api/apiproducts/mapper/ProductMapper.java
+++ src/main/java/com/mlt/api/apiproducts/mapper/ProductMapper.java
@@ -3,17 +3,17 @@ package com.mlt.api.apiproducts.mapper;
 import com.mlt.api.apiproducts.domain.dto.data.ProductDTO;
 import com.mlt.api.apiproducts.domain.dto.request.body.CreateProductRequest;
 import com.mlt.api.apiproducts.model.Product;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring", uses = {
-        PriceMapper.class, CategoryMapper.class, ProductCategoryMapper.class, ProductImageMapper.class
-})
-public interface ProductMapper {
+@Mapper(componentModel = "spring", uses = {PriceMapper.class, CategoryMapper.class, ProductCategoryMapper.class, ProductImageMapper.class})
+public abstract class ProductMapper {
 
     @Mapping(target = "categories", source = "productCategories")
     @Mapping(target = "images", source = "images")
-    ProductDTO toProductDTO(Product product);
+    public abstract ProductDTO toProductDTO(Product product);
 
     @Mapping(target = "deletedAt", ignore = true)
     @Mapping(target = "id", ignore = true)
@@ -21,7 +21,14 @@ public interface ProductMapper {
     @Mapping(target = "images", source = "images")
     @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
-    Product toProduct(CreateProductRequest productDTO);
+    public abstract Product toProduct(CreateProductRequest productDTO);
+
+    @AfterMapping
+    public Product mapProductInPrices(@MappingTarget Product.ProductBuilder product) {
+        Product result = product.build();
+        result.getPrices().forEach(p -> p.setProduct(result));
+        return result;
+    }
 
 
 }
