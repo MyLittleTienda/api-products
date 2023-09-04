fa83cf0c4b651e996d0379262e321b19a1e0c44a
RustyCrazyPunky <crazypunky@protonmail.com>
2023-09-04 15:02:58 -0300

    feat: Add config server y eureka server

--- src/main/java/com/mlt/api/ApiProductsApplication.java
+++ src/main/java/com/mlt/api/ApiProductsApplication.java
@@ -2,10 +2,8 @@ package com.mlt.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ApiProductsApplication {
 
     public static void main(String[] args) {
